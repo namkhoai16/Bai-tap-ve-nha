80d28d0304da35fcbceeaa47eca42c956ce8d7c6
Nam Nguyễn <namkhoai16@gmail.com>
2020-04-23 23:05:53 +0700

    first commit

---
+++ BT5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nhanvien nv1 = new nhanvien();
+            nv1.nhap();
+            nv1.xuat();
+            Console.Read();
+        }
+    }
+}
